970ad442164ac1e5c3c536f1285eb2e93a54067e
federus rudi <federusrudi@gmail.com>
2025-07-25 10:03:30 +0700

    delete: hamburger.js file because it has been added to the js folder

--- hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamburgerIcon = document.querySelector("#icon");
-const Menu = document.querySelector(".menu-hamburger");
-
-hamburgerIcon.addEventListener("click", function () {
-  Menu.classList.toggle("active");
-});
-  
