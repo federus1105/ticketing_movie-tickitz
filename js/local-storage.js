226c3ee188fe9594be10a7da93617c57e38df459
federus rudi <federusrudi@gmail.com>
2025-07-25 20:57:10 +0700

    fix: local-storage.js file validation logic for active users

--- js/local-storage.js
+++ js/local-storage.js
@@ -1,13 +1,24 @@
-const loggedln = localStorage.getItem(`loggedln`);
+const profile = document.querySelector("#profile-image");
+const logout = document.querySelector("#logout");
 
-if (loggedln !== `true`) {
-  window.location.href = "sign-in.html";
+profile.addEventListener("click", function () {
+  logout.classList.toggle("active");
+});
+
+const user = JSON.parse(localStorage.getItem("user"));
+const authButtons = document.getElementById("auth-buttons");
+const profil = document.getElementById("profile");
+const logoutBtn = document.getElementById("logout");
+
+if (user) {
+  authButtons.style.display = "none";
+  profile.style.display = "block";
 } else {
-  if (loggedln) {
-    const profil = document.querySelector(".profile");
-    const btn = document.querySelector(`btn.login`);
-
-    profil.classList.add(`profil-aktif`);
-    btn.classList.add(`btn-hidden`);
-  }
+  authButtons.style.display = "block";
+  profile.style.display = "none";
 }
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("user");
+  window.location.href = "sign-in.html";
+});
